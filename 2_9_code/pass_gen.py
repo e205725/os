3db30041d754a116a21213289dd7a4b60a457dbe
e205725 <74218209+e205725@users.noreply.github.com>
2022-11-14 21:51:59 +0900

    Delete pass_gen.py

--- 2_9_code/pass_gen.py
+++
@@ -1,8 +0,0 @@
-import string
-import secrets
-
-def pass_gen(size=12):
-   chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
-   return ''.join(secrets.choice(chars) for x in range(size))
-for i in range(10):
-   print(pass_gen(10))
